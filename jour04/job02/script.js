55610e62e6a7d1613a80e9393529b83839b14344
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-03-30 13:00:07 +0200

    commit jour04 job02

---
+++ jour04/job02/script.js
@@ -0,0 +1,11 @@
+var url = 'data.json';
+
+function jsonValueKey(test) {
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            return console.log(response[test])
+        });
+}
+
+jsonValueKey("city")
