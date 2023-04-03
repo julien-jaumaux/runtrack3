ecbcd3a71b764bf362f0c6ec7bf577728bf04713
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-04-03 12:37:29 +0200

    commit jour06 job04

---
+++ jour06/job04/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function loaded() {
+
+    var buttonBurger = document.getElementsByClassName('burger-icon');
+    var buttonNav = buttonBurger[0];
+    var nav = document.querySelector('nav');
+    
+    buttonNav.addEventListener("click", function(){
+        nav.classList.toggle("open");
+        });
+})
